bc046ff28d96c12c2eabcb91b6d0f11959fb63e2
justinww04 <justinw.whitman@gmail.com>
2024-03-14 06:12:35 -0400

    done

--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -1,34 +1,57 @@
-import React from 'react' // 👈 you'll need the reducer hook
+import React from 'react' 
+import { useReducer } from 'react'
 
-// 👇 these are the types of actions that can change state
+
 const CHANGE_INPUT = 'CHANGE_INPUT'
 const RESET_FORM = 'RESET_FORM'
 
-// 👇 create your initial state object here
+const initialState = {
+  authorName:'',
+  quoteText: '',
+}
 
-// 👇 create your reducer function here
+const reducer = (state, action) => {
+  switch (action.type) {
+    case CHANGE_INPUT: {
+      const { name, value} = action.payload
+      return {...state, [name]: value}
+    }
+    case RESET_FORM: {
+      return { authorName: '', quoteText: ''}
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+
 
 export default function TodoForm({ createQuote = () => { } }) {
-  // 👇 use the reducer hook to spin up state and dispatch
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const onChange= ({ target: {name, value}}) => {
+    dispatch ({ type: CHANGE_INPUT, payload: {name, value}})
+  }
 
-  const onChange = () => {
-    // 👇 implement
-  }
+ 
   const resetForm = () => {
-    // 👇 implement
+    dispatch ({type: RESET_FORM})
   }
-  const onNewQuote = () => {
-    // 👇 implement
+  const onNewQuote = (evt) => {
+    evt.preventDefault()
+    const { authorName, quoteText} = state
+    createQuote({ authorName, quoteText})
     resetForm()
   }
 
-  // 👇 some props are missing in the JSX below:
+  
   return (
     <form id="quoteForm" onSubmit={onNewQuote}>
       <h3>New Quote Form</h3>
       <label><span>Author:</span>
         <input
           type='text'
+          value= {state.authorName}
           name='authorName'
           placeholder='type author name'
           onChange={onChange}
@@ -37,6 +60,7 @@ export default function TodoForm({ createQuote = () => { } }) {
       <label><span>Quote text:</span>
         <textarea
           type='text'
+          value={state.quoteText}
           name='quoteText'
           placeholder='type quote'
           onChange={onChange}
